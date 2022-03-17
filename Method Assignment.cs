de60fbd902936a7377f71df824ae431c2c3364f8
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2022-03-16 22:05:27 -0300

    1st commit

---
+++ Method Assignment.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace C_Sharp_Blog
+public class RefLocal
+//Ref Method
+public static ref double ref local (double (] m) f
+return ref m[1);
+//main()
+static void Main(string[] args)
+I
+//Array{2] of double
+double() x = (2.1, 3.4) ;
+//Let's print the contents
+2.1.
+3.4
+Console.WriteLine("InThe values are:") ;
+foreach (double a in x) ( Console.WriteLine (a) ; |
+/Ref Local- By reference,
+to the second index
+ref var i = ref ref local (x);
+//Let's change the value at this reference
+//This will change the value at xt]!
+i=1.1:
+/It is now 2/1
+1.1
+Console.WriteLine ("InThe values are now: ");
+foreach (double a in x) { Console.WriteLine (a) ; }
+}
+}
+}
